1e2b1732209a5cbd2292aa1b40e4887ad6dac937
03332387456 <sahirmavia@gmail.com>
2023-03-31 11:14:27 +0500

    this is a java script

---
+++ app.js
@@ -0,0 +1,78 @@
+alert("Hello world");
+
+var a = "10"
+var b = "20"
+var c = a + b
+console.log(c)
+
+var a = "10"
+var b = "20"
+var c = a-b
+console.log(c)
+
+var a = "10"
+var b = "20"
+var c = a/b
+console.log(c)
+
+var a = "10"
+var b = "20"
+var c = a*b
+console.log(c)
+
+var a = "10" 
+var b = "20"
+var c = 5
+var d = (a + b)*c
+console.log(d)
+
+var a = 10 
+var b = 20 
+var c = a + b
+console.log(c)
+
+var a = "Ahtisham"
+var b = "Ansari"
+var c = a + b
+console.log(c)
+
+// var asd = weqq    //legal
+// var qwe123 = qwer  //legal
+// var 12qeee = adsf  //illegal
+// var qw-e = bcvd   //illegal
+// var $asd = wdwed //legal
+// var $123 = ewqew  //legal
+// var qwe_q = qwdccv   //legal
+// var qwr? = asdf  //illegal
+// var ?awd = fygcv  //illegal
+
+
+var warning = "do not close" 
+console.log(warning)
+
+var a = "12"
+var b = "22"
+var c = a + " " + " " + b
+console.log(c)
+
+var First_name = prompt ("Enter first name")
+var Last_name = prompt ("Enter last name")
+var City_name = prompt ("Enter city name")
+console.log(First_name + " " + Last_name + " From " + City_name + " " )
+
+
+if( IftaarPartyHAi = true){
+    alert("Ajao")
+}
+else{
+    alert("nahi Aao")
+}
+
+
+
+if( AssignmentBanGaya = true){
+    console.log("Submit kro")
+}
+else{
+    console.log("Nahi kro")
+}
